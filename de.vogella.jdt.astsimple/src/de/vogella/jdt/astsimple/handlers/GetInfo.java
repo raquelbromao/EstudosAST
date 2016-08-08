2f27df969e7140b122b50376a07aabc83513cc78
raquelbromao <raquelbromao@gmail.com>
2016-08-08 15:08:32 -0300

    Novo método e comentários

--- de.vogella.jdt.astsimple/src/de/vogella/jdt/astsimple/handlers/GetInfo.java
+++ de.vogella.jdt.astsimple/src/de/vogella/jdt/astsimple/handlers/GetInfo.java
@@ -90,91 +90,16 @@ public class GetInfo extends AbstractHandler {
 			parse.accept(visitor);
 			// Imprime na tela o nome do método e o tipo de retorno
 			for (MethodDeclaration method : visitor.getMethods()) {
-				System.out.println("Method name: " + method.getName() + "\nReturn type: " + method.getReturnType2()
+				System.out.println("Method name: " + method.getName() 
+						+ "\nReturn type: " + method.getReturnType2()
 						+ "\nAnother GetClass information: " + method.getClass()
-						+ "\nAnother2 ReceiverType Informarion: " + method.getReceiverType() + "\nReturn body:"
-						+ method.getBody() + "\n\n");
+						+ "\nAnother2 ReceiverType Informarion: " + method.getReceiverType() 
+						+ "\nReturn body:" + method.getBody() + "\n\n");
 			}
 
-			// Chamada em loop para análise do corpo dos métodos
-			for (IPackageFragment mypackage : packages) {
-				IType[] allTypes = unit.getAllTypes();
-
-				// Lista todas as classes do projeto, independente se estiver no
-				// mesmo arquivo ou não
-				for (IType type : allTypes) {
-					System.out.println("\nClass Name: " + type.getElementName() + " / " + type.getFullyQualifiedName());
-					String sourceCode = type.getSource();
-
-					ASTParse(sourceCode, type);
-				}
+			
 			}
 		}
-	}
-
-	private void ASTParse(String sourceCode, IType type) {
-		ASTParser parser = ASTParser.newParser(AST.JLS3);
-
-		parser.setSource(sourceCode.toCharArray());
-		parser.setKind(ASTParser.K_COMPILATION_UNIT);
-
-		final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
-
-		cu.accept(new ASTVisitor() {
-
-			Set names = new HashSet();
-
-			public boolean visit(MethodDeclaration node) {
-				SimpleName name = node.getName();
-				this.names.add(name.getIdentifier());
-				System.out.println("    Declaration of '" + name.getFullyQualifiedName() + "' at line"
-						+ cu.getLineNumber(name.getStartPosition()));
-				
-				
-				methodsDec.add(node);
-
-				/*
-				 * MethodDec metodoDec = new MethodDec();
-				 * metodoDec.setName(name.getFullyQualifiedName());
-				 * metodoDec.setClassOrg(type.getElementName());
-				 */
-
-				Block body = node.getBody();
-				body.accept(new ASTVisitor() {
-
-					Set names = new HashSet();
-
-					public boolean visit(MethodInvocation node) {
-						SimpleName name = node.getName();
-
-						this.names.add(name.getIdentifier());
-						System.out.println("     Invocation of '" + name.getFullyQualifiedName() + "' at line"
-								+ cu.getLineNumber(name.getStartPosition()));
-
-						/*
-						 * MethodDec metodoInv = new MethodDec();
-						 * metodoInv.setName(name.getFullyQualifiedName());
-						 * metodoInv.setClassOrg(name.getParent().toString());
-						 * metodoDec.setMethodsInvocates(metodoInv);
-						 */
-						return false; // do not continue
-					}
-				});
-
-				// methodsDec.add(metodoDec);
-
-				return false; // do not continue
-			}
-
-			public boolean visit(SimpleName node) {
-				if (this.names.contains(node.getIdentifier())) {
-					System.out.println("Usage of '" + node + "' at line " + cu.getLineNumber(node.getStartPosition()));
-				}
-				return true;
-			}
-		});
-
-	}
 
 	/**
 	 * Reads a ICompilationUnit and creates the AST DOM for manipulating the
--- de.vogella.jdt.infos/src/de/vogella/jdt/infos/handlers/SampleHandler.java
+++ de.vogella.jdt.infos/src/de/vogella/jdt/infos/handlers/SampleHandler.java
@@ -19,118 +19,155 @@ import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
-
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jface.text.Document;
 
 /**
  * 
- *	ORDEM DAS CHAMADAS DOS MÉTODOS
- * 1. execute(ExecutionEvent event)
- * 2. printProjectInfo(project)
- * 3. printPackageInfos(javaProject)
- * 4. printICompilationUnitInfo(mypackage)
- * 5. printCompilationUnitDetails(unit)
- * 6. printIMethods(unit)
- * 7. printIMethodDetails(type)
+ * ORDEM DAS CHAMADAS DOS MÉTODOS 1. execute(ExecutionEvent event) 2.
+ * printProjectInfo(project) 3. printPackageInfos(javaProject) 4.
+ * printICompilationUnitInfo(mypackage) 5. printCompilationUnitDetails(unit) 6.
+ * printIMethods(unit) 7. printIMethodDetails(type)
  *
  */
 
 public class SampleHandler extends AbstractHandler {
-		
-		//////////////////
-		//////EXECUTE////
-		/////////////////
-        public Object execute(ExecutionEvent event) throws ExecutionException {
-                // Pega a raiz da workspace
-                IWorkspace workspace = ResourcesPlugin.getWorkspace();
-                IWorkspaceRoot root = workspace.getRoot();
-                
-                // Pega todos os projetos da workspace
-                IProject[] projects = root.getProjects();
-                
-                // Faz um loop sobre todos os projetos e chama o método printProjectInfo p/ cada projeto
-                for (IProject project : projects) {
-                        try {
-                        	// Chamada do método em cada projeto da workspace
-							printProjectInfo(project);
-						} catch (JavaModelException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						} catch (CoreException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-                }
-                return null;
-        }
 
-		////////////////////////////
-		//////PRINT PROJECT INFO////
-		////////////////////////////
-        private void printProjectInfo(IProject project) throws CoreException, JavaModelException {
-        		// Imprime o nome dos projetos
-                System.out.println("Working in project " + project.getName());
-                
-                // Checa se há um projeto Java
-                IJavaProject javaProject = JavaCore.create(project);
-                printPackageInfos(javaProject);
-        }
+	//////////////////
+	////// EXECUTE////
+	/////////////////
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// Pega a raiz da workspace
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		IWorkspaceRoot root = workspace.getRoot();
 
-		////////////////////////////
-		//////PRINT PACKAGE INFO////
-		////////////////////////////
-        private void printPackageInfos(IJavaProject javaProject) throws JavaModelException {
-                IPackageFragment[] packages = javaProject.getPackageFragments();
-                for (IPackageFragment mypackage : packages) {
-                        // Package fragments include all packages in the
-                        // classpath
-                        // We will only look at the package from the source
-                        // folder
-                        // K_BINARY would include also included JARS, e.g.
-                        // rt.jar
-                        if (mypackage.getKind() == IPackageFragmentRoot.K_SOURCE) {
-                        		// Imprime nome do pacote
-                                System.out.println("Package " + mypackage.getElementName());
-                                printICompilationUnitInfo(mypackage);
-                        }
-                }
-        }
+		// Pega todos os projetos da workspace
+		IProject[] projects = root.getProjects();
 
-		/////////////////////////////////////
-		//////PRINT COMPILATION UNIT INFO////
-		/////////////////////////////////////
-        private void printICompilationUnitInfo(IPackageFragment mypackage) throws JavaModelException {
-                for (ICompilationUnit unit : mypackage.getCompilationUnits()) {
-                        printCompilationUnitDetails(unit);
-                }
-        }
+		System.out.println("===================================================");
+		System.out.println("===================================================");
 
-        private void printCompilationUnitDetails(ICompilationUnit unit) throws JavaModelException {
-        		// Imprime nome do arquivo fonte java
-                System.out.println("Source file " + unit.getElementName());
-                Document doc = new Document(unit.getSource());
-                // Imprime o número de linhas do documento java
-                System.out.println("Has number of lines: " + doc.getNumberOfLines() + "\n");
-                printIMethods(unit);
-        }
-        
-		////////////////////////////
-		//////PRINT METHODS INFO////
-		////////////////////////////
-        private void printIMethods(ICompilationUnit unit) throws JavaModelException {
-            IType[] allTypes = unit.getAllTypes();
-            for (IType type : allTypes) {
-                    printIMethodDetails(type);
-            }
-        }
+		// Faz um loop sobre todos os projetos e chama o método printProjectInfo
+		// p/ cada projeto
+		for (IProject project : projects) {
+			try {
+				// Chamada do método em cada projeto da workspace
+				printProjectInfo(project);
+				System.out.println("===================================================");
+				System.out.println("===================================================");
+			} catch (JavaModelException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (CoreException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
 
-        private void printIMethodDetails(IType type) throws JavaModelException {
-                IMethod[] methods = type.getMethods();
-                for (IMethod method : methods) {
-                		//Imprime as informações dos métodos presentes no arquivo fonte java
-                        System.out.println("Method name " + method.getElementName());
-                        System.out.println("Signature " + method.getSignature());
-                        System.out.println("Return Type " + method.getReturnType() + "\n");
-                }
-        }
+	////////////////////////////
+	////// PRINT PROJECT INFO////
+	////////////////////////////
+	private void printProjectInfo(IProject project) throws CoreException, JavaModelException {
+		// Imprime o nome dos projetos
+		System.out.println("Working in project " + project.getName() + "\n");
+
+		// Checa se há um projeto Java
+		IJavaProject javaProject = JavaCore.create(project);
+		printPackageInfos(javaProject);
+	}
+
+	////////////////////////////
+	////// PRINT PACKAGE INFO////
+	////////////////////////////
+	private void printPackageInfos(IJavaProject javaProject) throws JavaModelException {
+		IPackageFragment[] packages = javaProject.getPackageFragments();
+		for (IPackageFragment mypackage : packages) {
+			// Package fragments include all packages in the classpath
+			// We will only look at the package from the source folder
+			// K_BINARY would include also included JARS, e.g. rt.jar
+			if (mypackage.getKind() == IPackageFragmentRoot.K_SOURCE) {
+				// Imprime nome do pacote
+				System.out.println("Package " + mypackage.getElementName() + "\n");
+				printICompilationUnitInfo(mypackage);
+				createASTmethod(mypackage);
+			}
+		}
+	}
+
+	/////////////////////////////////////
+	////// PRINT COMPILATION UNIT INFO////
+	/////////////////////////////////////
+	private void printICompilationUnitInfo(IPackageFragment mypackage) throws JavaModelException {
+		for (ICompilationUnit unit : mypackage.getCompilationUnits()) {
+			printCompilationUnitDetails(unit);
+		}
+	}
+
+	private void printCompilationUnitDetails(ICompilationUnit unit) throws JavaModelException {
+		// Imprime nome do arquivo fonte java
+		System.out.println("Source file " + unit.getElementName());
+		Document doc = new Document(unit.getSource());
+		// Imprime o número de linhas do documento java
+		System.out.println("Has number of lines: " + doc.getNumberOfLines());
+		printIMethods(unit);
+	}
+
+	////////////////////////////
+	////// PRINT METHODS INFO////
+	////////////////////////////
+	private void printIMethods(ICompilationUnit unit) throws JavaModelException {
+		IType[] allTypes = unit.getAllTypes();
+		for (IType type : allTypes) {
+			printIMethodDetails(type);
+		}
+	}
+
+	private void printIMethodDetails(IType type) throws JavaModelException {
+		IMethod[] methods = type.getMethods();
+		for (IMethod method : methods) {
+			// Imprime as informações dos métodos presentes no arquivo fonte
+			// java
+			System.out.println("\nMethod name " + method.getElementName());
+			System.out.println("Signature " + method.getSignature());
+			System.out.println("Return Type " + method.getReturnType());
+		}
+	}
+	
+	/**
+	 * Reads a ICompilationUnit and creates the AST DOM for manipulating the
+	 * Java source file
+	 *
+	 * @param unit
+	 * @return
+	 */
+
+	private static CompilationUnit parse(ICompilationUnit unit) {
+		ASTParser parser = ASTParser.newParser(AST.JLS8);
+		parser.setKind(ASTParser.K_COMPILATION_UNIT);
+		parser.setSource(unit);
+		parser.setResolveBindings(true);
+		return (CompilationUnit) parser.createAST(null); // parse
+	}
+
+	private void createASTmethod(IPackageFragment mypackage) throws JavaModelException {
+		for (ICompilationUnit unit : mypackage.getCompilationUnits()) {
+			// now create the AST for the ICompilationUnits
+			CompilationUnit parse = parse(unit);
+			MethodVisitor visitor = new MethodVisitor();
+			parse.accept(visitor);
+			// Imprime na tela o nome do método e o tipo de retorno
+			for (MethodDeclaration method : visitor.getMethods()) {
+				System.out.println("Method name: " + method.getName() + "\nReturn type: " + method.getReturnType2()
+						+ "\nAnother GetClass information: " + method.getClass()
+						+ "\nAnother2 ReceiverType Informarion: " + method.getReceiverType() + "\nReturn body:"
+						+ method.getBody() + "\n\n");
+			}
+
+		}
+	}
 }

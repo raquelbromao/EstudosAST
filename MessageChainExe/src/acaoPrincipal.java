ac4e514d51a5e916303419642491ac305e59161a
raquelbromao <raquelbromao@gmail.com>
2016-08-09 14:49:53 -0300

    Mudança no uso do do-while

--- MessageChainExe/src/acaoPrincipal.java
+++ MessageChainExe/src/acaoPrincipal.java
@@ -1,47 +1,57 @@
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-public class Teste {
+public class acaoPrincipal {
 
 	public static void main(String[] args) {
 		// Vari·veis globais
 		Scanner ler = new Scanner(System.in);
 		String messagechain;
-		String resposta;
-		
+		int resposta;
+		boolean continua;
+
+		// a().b().c().d().f()
+
 		do {
 			// Pede o Message Chain
 			System.out.printf("Informe o Message Chain:\n");
 			messagechain = ler.nextLine();
-			
+
 			// Mostra o Message Chain na tela e d· seu tamanho
-			System.out.println("Message Chain: " + messagechain);
+			System.out.println("\nMessage Chain: " + messagechain);
 			System.out.println("Tamanho: " + messagechain.length());
 
 			// QUEBRA DA STRING E RECONHECER QUE … UM MESSAGE CHAIN
 			if (messagechain.contains(".") == true) {
-				System.out.println("… Message Chain!");
-				
+				System.out.println("\n… Message Chain!");
+
 				// Quebra a vari·vel quando acha . e armazena no array aux
-				String[] aux = messagechain.split(Pattern.quote ("."));
-				
-				// Pega o tamanho da string aux				
+				String[] aux = messagechain.split(Pattern.quote("."));
+
+				// Pega o tamanho da string aux
 				// Imprime a vari·vel aux na tela
 				for (int i = 0; i < aux.length; i++) {
-					System.out.println("String["+i+"] :"+aux[i]);
+					System.out.println("String[" + i + "] :" + aux[i]);
 				}
-				
+
 			} else {
-				System.out.println("N„o È Message Chain!");
+				System.out.println("\nN„o È Message Chain!");
 			}
+
+			// Pergunta se quer continuar testando
+			System.out.println("\nOutro teste? 0 SIM // 1 N√O");
+			resposta = ler.nextInt();
 			
+			if (resposta == 1) {
+				System.out.println("Teste finalizado!\n");
+				continua = false;
+			} else {
+				continua = true;
+			}
+
 			// Reinicializa a vari·vel para evitar erros
 			messagechain = null;
-			
-			// Pergunta se quer continuar testando
-			System.out.println("Outro teste?");
-			resposta = ler.nextLine();
-			
-		} while (resposta != "n");
+
+		} while (continua);
 	}
 }

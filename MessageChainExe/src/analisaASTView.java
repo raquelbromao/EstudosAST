2ac19c1810eb8eeeccf38001dee4464e2cd1a114
raquelbromao <raquelbromao@gmail.com>
2016-08-12 15:40:55 -0300

    Criação de classe para análise de nós do AST com auxílio do ASTView

---
+++ MessageChainExe/src/analisaASTView.java
@@ -0,0 +1,33 @@
+
+public class analisaASTView {
+	public int somadiferente(int a, int b) {
+		int soma;
+		soma = a + b;
+		return soma;
+	}
+
+	public int subdiferente(int a, int b) {
+		int sub = 0;
+		if (a > b)
+			sub = a - b;
+		if (b > a)
+			sub = b - a;
+		return sub;
+	}
+
+	public int multdiferente(int a, int b) {
+		int mult;
+		mult = a * b;
+		return mult;
+	}
+
+	public int raizdiferente() {
+		return 0;
+	}
+
+	public void chamadacadeia() {
+		Object a;
+		a.somadiferente().subdiferente().multdiferente().raizdiferente();
+	}
+
+}

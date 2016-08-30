b5c56229ced42402d4645cfd8d91aed135f5dece
raquelbromao <raquelbromao@gmail.com>
2016-08-30 08:28:17 -0300

    Nova classe p/ aplicação de recursividade no sistema de message chain

--- MessageChainExe/src/alg/AcaoRecursivo.java
+++ MessageChainExe/src/alg/AcaoRecursivo.java
@@ -2,10 +2,10 @@ package alg;
 
 import java.util.regex.Pattern;
 
-public class acaoPrincipal {
+public class AcaoRecursivo {
 	
 	// String p/ testes que o algoritmo N√O deve cobrir
-	public static final String[] testeErro = {"objeto", ".objeto", "objeto.", "objeto;",
+	/*public static final String[] testeErro = {"objeto", ".objeto", "objeto.", "objeto;",
 			"objeto.function", "objeto.function;", ";objeto.function", "objeto.;function", "objeto;.function",
 			"objeto()", ".objeto()", "objeto.()", "objeto().",
 			"objeto().function", "objeto.()function","()objeto.function",
@@ -22,10 +22,10 @@ public class acaoPrincipal {
 			"objeto.metodo1(param1.class).metodo2((param2.class);",
 			"objeto.metodo1((param1.class)).metodo2(param2.class);",
 			"objeto.metodo1(param1.class).metodo2(param2.class));",
-			"objeto.metodo1(param1.clas).metodo2(param2.clas));",""};
+			"objeto.metodo1(param1.clas).metodo2(param2.clas));",""};*/
 	
 	// String p/ testes que o algoritmo DEVE cobrir
-	public static final String[] testeValido = {"objeto.function().function2();",
+	public static final String[] testeValido = {"objeto.function(function3().function4()).function2();",
 			"objeto.function().function2().function3();",
 			"objeto.function().function2().function3().function4();", 
 			"objeto.function().function2().function3().function4().function5();",
@@ -57,6 +57,55 @@ public class acaoPrincipal {
 			"BasicEnemy.class.getConstructor(Map.class, Path.class);", 
 			"mainMenuScene.getStylesheets().addAll(this.getClass().getResource('style.css').toExternalForm());"};
 	
+	public static void splitMessageChainRec (String s, int j) {
+		String[] auxrec = null;
+		
+
+		// Imprime a vari·vel aux na tela, separando os componentes do message chain analisado
+		if (j == 0) {
+			// retira o ";" do final da string, substituindo por espaÁo em branco	
+			s = s.replace(";", " ");	
+			
+			// Quebra a vari·vel quando acha "." e armazena o resto numa posiÁ„o do array aux
+			// a().b() -> "." È descartado e "a()" fica em aux[0] e "b()" em aux[1]
+			auxrec = s.split(Pattern.quote("."));		
+			
+			if (auxrec[0].equals("this") || auxrec[0].equals("This")) {
+				System.out.println("\tThis -> " + auxrec[0]);	
+				System.out.println("\tObjeto: " + auxrec[1]);	
+				for (int i = 2; i < auxrec.length; i++) {
+					System.out.println("\tMÈtodo[" + (i - 1) + "]: " + auxrec[i]);
+					verificaMessageChainRec(auxrec[i]);
+				}
+			} else {
+				System.out.println("\tObjeto: " + auxrec[0]);		
+				for (int i = 1; i < auxrec.length; i++) {
+					System.out.println("\tMÈtodo[" + i + "]: " + auxrec[i]);	
+					verificaMessageChainRec(auxrec[i]);
+				}
+			}		
+		} else if (j == 1) {
+			// retira o ";" do final da string, substituindo por espaÁo em branco	
+			s = s.replace(";", " ");	
+			
+			// Quebra a vari·vel quando acha um "." que N√O SEJA seguido de "class" e
+			// armazena o resto numa posiÁ„o do array aux
+			// a().b() -> "." È descartado e "a()" fica em aux[0] e "b()" em aux[1]
+			// BUG: est· cortando "m" junto com ".", ou seja, cortando ".m"
+			auxrec = s.split("[\\.][^class]"); 
+			System.out.println("\tObjeto: " + auxrec[0]);	
+			for (int i = 1; i < auxrec.length; i++) {
+				System.out.println("\tMÈtodo[" + i + "]: " + auxrec[i]);
+				verificaMessageChainRec(auxrec[i]);
+			}
+		} else if (j == 5) {
+			System.out.println("\tMÈtodo: " + s);
+		}
+		
+		// reinicializa a vari·vel aux liberando a memÛria
+		auxrec = null;
+	}
+	
 	public static void splitMessageChain (String s, int j) {
 		String[] aux = null;
 		
@@ -74,13 +123,13 @@ public class acaoPrincipal {
 				System.out.println("Objeto: " + aux[1]);	
 				for (int i = 2; i < aux.length; i++) {
 					System.out.println("MÈtodo[" + (i - 1) + "]: " + aux[i]);
-					//verificaMessageChain(aux[i]);
+					verificaMessageChainRec(aux[i]);
 				}
 			} else {
 				System.out.println("Objeto: " + aux[0]);		
 				for (int i = 1; i < aux.length; i++) {
 					System.out.println("MÈtodo[" + i + "]: " + aux[i]);	
-					//verificaMessageChain(aux[i]);
+					verificaMessageChainRec(aux[i]);
 				}
 			}		
 		} else if (j == 1) {
@@ -92,12 +141,56 @@ public class acaoPrincipal {
 			System.out.println("Objeto: " + aux[0]);	
 			for (int i = 1; i < aux.length; i++) {
 				System.out.println("MÈtodo[" + i + "]: " + aux[i]);
-				//verificaMessageChain(aux[i]);
+				verificaMessageChainRec(aux[i]);
 			}
 		}
 		
 		// reinicializa a vari·vel aux liberando a memÛria
 		aux = null;
+	}
+	
+	public static void verificaMessageChainRec (String s) {		
+		if (s!=null && s.matches("[\\w]+([\\.]{1}[\\w]+[(][\\w]*([\\s]*[,][\\s]*[\\w]+)*[)]){2,}[;]")) {
+			/*
+			 * EXPLICA«√O REGEX:
+			 * 
+			 * CASO 1: objeto.function1().function2()...functionN();
+			 * CASO 2: objeto.function1(param1).function2()...functionN();
+			 * CASO 3: objeto.function1().function2(param2)...functionN();
+			 * CASO 4: objeto.function1(param1).function2(param2)...functionN(paramN);
+			 * CASO 5: CASO 1: objeto.function1().function2(param2,param3)...functionN(paramN1,paramN2,paramN3);
+			 * 
+			 */
+			//System.out.println("\n… Message Chain para "+s+"\n");
+			splitMessageChainRec(s,0); 
+		} else if (s!=null && s.matches("([tT]his)[\\.][\\w]+([\\.][\\w]+[(][\\w]*([\\s]*[,][\\s]*[\\w]+)*[)]){2,}[;]")) {
+			/*
+			 * EXPLICA«√O REGEX:
+			 * 
+			 * CASO 1: [tT]his.objeto.function1().function2()...functionN();
+			 * CASO 2: [tT]his.function1().function2()...functionN();
+			 * 
+			 */
+			//System.out.println("\n… Message Chain para "+s+"\n");
+			splitMessageChainRec(s,0);
+		} else if (s!=null && s.matches("[\\w]+([\\.][\\w]+[(]([\\w]+([\\.](class))*([\\s]*[,][\\s]*[\\w]+([\\.](class))*)*)*[)]){2,}[;]")) {
+			/*
+			 * EXPLICA«√O REGEX:
+			 * 
+			 * CASO 1: objeto.function1().function2(param2.class,param3)...functionN(paramN1,paramN2.class,paramN3);
+			 * CASO 2: objeto.function1(param1.class, param2, param3.class).function2(param2,param3)...functionN(paramN1,paramN2.class,paramN3);
+			 * 
+			*/	
+			//System.out.println("\n… Message Chain para "+s+"\n");
+			splitMessageChainRec(s,1);
+		} else if (s.isEmpty()) { 
+			// Retorna true (0) qnd for vazia false (1) qnd for diferente de nula
+			System.out.println("\n\tPar‚metros vazia!\n");
+		} else if (s!=null && s.matches("[\\w]+[(][)]")) {
+			splitMessageChainRec(s,5);
+		} else {
+			System.out.println("\n\tN√O … MESSAGE CHAIN PARA "+s+"\n");	
+		}
 	}
 	
 	public static void verificaMessageChain (String s) {		
@@ -111,32 +204,6 @@ public class acaoPrincipal {
 			 * CASO 4: objeto.function1(param1).function2(param2)...functionN(paramN);
 			 * CASO 5: CASO 1: objeto.function1().function2(param2,param3)...functionN(paramN1,paramN2,paramN3);
 			 * 
-			 * [\\w]+ -> qlqr combinaÁ„o de caracteres numÈricos e/ou literais, repetindo  1 ou infinitas vezes
-			 * 
-			 * ... (G1){2,} -> grupo 1 que deve ser repetido 2 ou mais vezes
-			 *  
-			 * [\\.] -> necess·rio conter "." uma vez
-			 * 
-			 * [\\w]+ -> qlqr combinaÁ„o de caracteres numÈricos e/ou literais, repetindo  1 ou infinitas vezes
-			 * 
-			 * [(] -> necess·rio conter "(" uma vez
-			 * 
-			 * [\\w]* -> qlqr combinaÁ„o de caracteres numÈricos e/ou literais, 0 ou infinitas vezes
-			 *  
-			 * (G1 (G2)* G1) -> grupo 2 que deve ser repetido 0 ou infinitas vezes
-			 * 
-			 * [\\s]* -> necess·rio conter espaÁo em branco 0 ou infinitas vezes
-			 * 
-			 * [,] -> necess·rio conter o caractere "," uma vez
-			 * 
-			 * [\\s]* -> necess·rio conter espaÁo em branco 0 ou infinitas vezes
-			 * 
-			 * [\\w]+ -> qlqr combinaÁ„o de caracteres numÈricos e/ou literais, repetindo  1 ou infinitas vezes
-			 * 
-			 * [)] -> necess·rio conter ")" uma vez
-			 * 
-			 * [;] -> necess·rio o caractere ";" no final para ser aceito
-			 * 
 			 */
 			System.out.println("\n… Message Chain para "+s+"\n");
 			splitMessageChain(s,0); 
@@ -146,38 +213,6 @@ public class acaoPrincipal {
 			 * 
 			 * CASO 1: [tT]his.objeto.function1().function2()...functionN();
 			 * CASO 2: [tT]his.function1().function2()...functionN();
-			 * 
-			 * (G1)... -> grupo 1
-			 * 
-			 * [Tt]his -> This ou this uma ˙nica vez
-			 * 
-			 * [\\.] -> necess·rio conter "." uma vez
-			 * 
-			 * [\\w]+ -> qlqr combinaÁ„o de caracteres numÈricos e/ou literais, repetindo  1 ou infinitas vezes
-			 * 
-			 * (G1) ... (G2){2,} -> grupo 2 que deve ser repetido 2 ou mais vezes
-			 * 
-			 * [\\.] -> necess·rio conter "." uma vez
-			 * 
-			 * [\\w]+ -> qlqr combinaÁ„o de caracteres numÈricos e/ou literais, repetindo  1 ou infinitas vezes
-			 * 
-			 * [(] -> necess·rio conter "(" uma vez
-			 * 
-			 * [\\w]* -> qlqr combinaÁ„o de caracteres numÈricos e/ou literais, 0 ou infinitas vezes
-			 * 
-			 * (G1)...(G2 (G3)* G2) -> grupo 3 que deve ser repetido 0 ou infinitas vezes
-			 * 
-			 * [\\s]* -> necess·rio conter espaÁo em branco 0 ou infinitas vezes
-			 * 
-			 * [,] -> necess·rio conter o caractere "," uma vez
-			 * 
-			 * [\\s]* -> necess·rio conter espaÁo em branco 0 ou infinitas vezes
-			 * 
-			 * [\\w]+ -> qlqr combinaÁ„o de caracteres numÈricos e/ou literais, repetindo  1 ou infinitas vezes 
-			 *  
-			 * [)] -> necess·rio conter ")" uma vez
-			 * 
-			 * [;] -> necess·rio o caracter ";" no final para ser aceito
 			 * 
 			 */
 			System.out.println("\n… Message Chain para "+s+"\n");
@@ -189,45 +224,7 @@ public class acaoPrincipal {
 			 * CASO 1: objeto.function1().function2(param2.class,param3)...functionN(paramN1,paramN2.class,paramN3);
 			 * CASO 2: objeto.function1(param1.class, param2, param3.class).function2(param2,param3)...functionN(paramN1,paramN2.class,paramN3);
 			 * 
-			 * [\\w]+ -> qlqr combinaÁ„o de caracteres numÈricos e/ou literais, repetindo  1 ou infinitas vezes
-			 * 
-			 * ... (G1){2,} -> grupo 1 que deve ser repetido 2 ou mais vezes
-			 * 
-			 * [\\.] -> necess·rio conter "." uma vez
-			 * 
-			 * [\\w]+ -> qlqr combinaÁ„o de caracteres numÈricos e/ou literais, repetindo  1 ou infinitas vezes
-			 * 
-			 * [(] -> necess·rio conter "(" uma vez
-			 * 
-			 * (G1 (G2)*) -> grupo 2 que deve ser repetido 0 ou infinitas vezes
-			 * 
-			 * [\\w]+ -> qlqr combinaÁ„o de caracteres numÈricos e/ou literais, repetindo  1 ou infinitas vezes
-			 * 
-			 * (G1 (G2 (G3)*)) -> grupo 3 que deve ser repetido 0 ou infinitas vezes
-			 * 
-			 * [\\.] -> necess·rio conter "." uma vez
-			 * 
-			 * (class) -> necess·rio conter "class" uma vez
-			 * 
-			 * (G1 (G2 (G3) (G4)*)) -> grupo 4 que deve ser repetido 0 ou infinitas vezes
-			 * 
-			 * [\\s]* -> necess·rio conter espaÁo em branco 0 ou infinitas vezes
-			 * 
-			 * [,] -> necess·rio conter o caractere "," uma vez
-			 * 
-			 * [\\s]* -> necess·rio conter espaÁo em branco 0 ou infinitas vezes
-			 * 
-			 * [\\w]+ -> qlqr combinaÁ„o de caracteres numÈricos e/ou literais, repetindo  1 ou infinitas vezes
-			 * 
-			 * (G1 (G2 (G3) (G4 (G5)))) -> grupo 5
-			 * 
-			 * [\\.] -> necess·rio conter "." uma vez
-			 * 
-			 * (class) -> necess·rio conter "class" uma vez 
-			 * 
-			 * [;] -> necess·rio o caracter ";" no final para ser aceito
-			 * 
-			 */
+			*/	
 			System.out.println("\n… Message Chain para "+s+"\n");
 			splitMessageChain(s,1);
 		} else if (s.isEmpty()) { 
@@ -245,8 +242,8 @@ public class acaoPrincipal {
 	}
 
 	public static void main(String[] args) {
-		testaStrings(testeErro);		
-		System.out.println("\n#####################################################\n");		
+		//testaStrings(testeErro);		
+		//System.out.println("\n#####################################################\n");		
 		testaStrings(testeValido);
 		System.out.println("\n#####################################################\n");	
 		testaStrings(testeExcecoes);

6e8c62122b9f1bf1461b05061d37dd42207f6ca7
raquelbromao <raquelbromao@gmail.com>
2016-09-01 11:42:49 -0300

    Manipulação e testes com indexOf e lastIndexOf

--- MessageChainExe/src/alg/TesteParenteses.java
+++ MessageChainExe/src/alg/TesteParenteses.java
@@ -5,8 +5,11 @@ import java.util.regex.Pattern;
 public class TesteParenteses {
 
 	public static void main(String[] args) {
-		String testeParenteses = "objeto.function1(this.objeto.function3()).function2(amor)";
+		String testeParenteses = "objeto.function1(this.objeto.function3()).function2(amor).function4()";
 		String[] split2 = null;
+	
+		System.out.println("Indíce da primeira ocorrência de '(': " + testeParenteses.indexOf("("));
+		System.out.println("Indíce da última ocorrência de ')': " + testeParenteses.lastIndexOf(")") + "\n");
 		
 		String[] split1 = testeParenteses.split(Pattern.quote(").")); 
 		System.out.println("###### PRIMEIRO SPLIT ");
@@ -14,12 +17,15 @@ public class TesteParenteses {
 			System.out.println("Componente["+ w +"]: " + split1[w]);
 		}		
 		
+		System.out.println("Indíce da primeira ocorrência de '(': " + split1[0].indexOf("("));
+		System.out.println("Indíce da última ocorrência de ')': " + split1[0].lastIndexOf(")"));
+		
 		System.out.println("\n\n###### SEGUNDO SPLIT ");
 		for (int j = 0; j < split1.length; j++) {
 			split2 = split1[j].split(Pattern.quote("("));
-			System.out.println("## Componentes de split ["+j+"]");
+			System.out.println("## Componentes de split1["+ j +"]");
 			for (int k = 0; k < split2.length; k++) {
-				System.out.println("Componente["+ k +"]: " +split2[k]);
+				System.out.println("Componente["+ k +"]: " + split2[k]);
 			}
 			System.out.print("\n");
 		}
